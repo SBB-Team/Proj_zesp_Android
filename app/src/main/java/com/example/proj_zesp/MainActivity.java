0775afb537a316a7a26d22843b6a3f8861685c03
Bemmen <dwodarion@gmail.com>
2021-03-14 18:59:24 +0200

    Log_screen: update

--- app/src/main/java/com/example/proj_zesp/MainActivity.java
+++ app/src/main/java/com/example/proj_zesp/MainActivity.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        EditText email = (EditText) findViewById(R.id.email);
+        email.setHintTextColor(getResources().getColor(R.color.black));
+        EditText password = (EditText) findViewById(R.id.password);
+        password.setHintTextColor(getResources().getColor(R.color.black));
     }
 }
