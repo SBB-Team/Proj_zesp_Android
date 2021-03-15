ce1e0de8e7a71468a709e0306e5e5ea184f460a1
BohdanSizomin <nbzp28@gmail.com>
2021-03-15 18:03:06 +0100

    <b>

--- app/src/main/java/com/example/proj_zesp/MainActivity.java
+++ app/src/main/java/com/example/proj_zesp/MainActivity.java
@@ -17,9 +17,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.sign_up_screen);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
+
+
         EditText email = (EditText) findViewById(R.id.email);
         email.setHintTextColor(getResources().getColor(R.color.black));
+
         EditText password = (EditText) findViewById(R.id.password);
+        password.setHintTextColor(getResources().getColor(R.color.black));
+
+        EditText password2 = (EditText) findViewById(R.id.password2);
+        password.setHintTextColor(getResources().getColor(R.color.black));
+
+        EditText first_name = (EditText) findViewById(R.id.first_name);
+        password.setHintTextColor(getResources().getColor(R.color.black));
+
+        EditText last_name = (EditText) findViewById(R.id.last_name);
         password.setHintTextColor(getResources().getColor(R.color.black));
     }
 }
