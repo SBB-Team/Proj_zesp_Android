15d42ed31c31c88ab79f0f90cfa828ddf7aae975
Bemmen <dwodarion@gmail.com>
2021-03-19 11:56:54 +0200

    GUI: little update

--- app/src/main/java/com/example/proj_zesp/SignUpScreenActivity.java
+++ app/src/main/java/com/example/proj_zesp/SignUpScreenActivity.java
@@ -3,6 +3,7 @@ package com.example.proj_zesp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -47,6 +48,8 @@ public class SignUpScreenActivity extends AppCompatActivity {
                 //onBackPressed();
             }
         });
+
+        //last_name.setPaintFlags(last_name.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
 
     }
