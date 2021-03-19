d860391f31ad85ee0f616d54cfb6cbd3ef5c297f
Bemmen <dwodarion@gmail.com>
2021-03-19 13:27:21 +0200

    GUI: update

--- app/src/main/java/com/example/proj_zesp/SignUpScreenActivity.java
+++ app/src/main/java/com/example/proj_zesp/SignUpScreenActivity.java
@@ -8,14 +8,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SignUpScreenActivity extends AppCompatActivity {
+
+    EditText email, password, password2, first_name, last_name;
+    TextView email_e, password_e, password2_e, fname_e, lname_e;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up_screen);
         //
+        TextView email_e = (TextView) findViewById(R.id.email_e);
+        TextView password_e = (TextView) findViewById(R.id.password_e);
+        TextView password2_e = (TextView) findViewById(R.id.password2_e);
+        TextView fname_e = (TextView) findViewById(R.id.fname_e);
+        TextView lname_e = (TextView) findViewById(R.id.lname_e);
+
+
         EditText email = (EditText) findViewById(R.id.email);
         email.setHintTextColor(getResources().getColor(R.color.black));
 
@@ -36,16 +49,50 @@ public class SignUpScreenActivity extends AppCompatActivity {
         signup_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), FirstStartScreen.class);
-                i.putExtra("email",email.getText().toString());
-                i.putExtra("password",email.getText().toString());
-                i.putExtra("first_name",email.getText().toString());
-                i.putExtra("last_name",email.getText().toString());
 
-                startActivity(i);
+                if (email.getText().toString().equals("") || email.getText().toString() == null){
+                    email_e.setText("Enter your email");
+                    email_e.setVisibility(View.VISIBLE);
+                }
+                else if(password.getText().toString().equals("") || password.getText().toString() == null){
+                    password_e.setText("Enter your password");
+                    password_e.setVisibility(View.VISIBLE);
+                    email_e.setVisibility(View.INVISIBLE);
+                }
+                else if(password2.getText().toString().equals("") || password2.getText().toString() == null){
+                    password2_e.setText("Repeat your password");
+                    password2_e.setVisibility(View.VISIBLE);
+                    password_e.setVisibility(View.INVISIBLE);
+                }
+                else if(!password2.getText().toString().equals(password.getText().toString())){
+                    password2_e.setText("Repeat your password properly");
+                    password2_e.setVisibility(View.VISIBLE);
+                    password_e.setVisibility(View.INVISIBLE);
+                }
+                else if(first_name.getText().toString().equals("") || first_name.getText().toString() == null){
+                    fname_e.setText("Enter your first name");
+                    fname_e.setVisibility(View.VISIBLE);
+                    password2_e.setVisibility(View.INVISIBLE);
+                }
+                else if(last_name.getText().toString().equals("") || last_name.getText().toString() == null){
+                    lname_e.setText("Enter your last name");
+                    lname_e.setVisibility(View.VISIBLE);
+                    fname_e.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    lname_e.setVisibility(View.INVISIBLE);
 
-                finish();
-                //onBackPressed();
+                    Intent i = new Intent(getApplicationContext(), FirstStartScreen.class);
+                    i.putExtra("email", email.getText().toString());
+                    i.putExtra("password", email.getText().toString());
+                    i.putExtra("first_name", email.getText().toString());
+                    i.putExtra("last_name", email.getText().toString());
+
+                    startActivity(i);
+
+                    finish();
+                    //onBackPressed();
+                }
             }
         });
 
