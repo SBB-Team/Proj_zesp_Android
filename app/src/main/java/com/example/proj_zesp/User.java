0870a529e98820026f41fd1ef1a70ab4cace7252
Bemmen <dwodarion@gmail.com>
2021-03-25 16:16:20 +0200

    User: creating class

---
+++ app/src/main/java/com/example/proj_zesp/User.java
@@ -0,0 +1,44 @@
+package com.example.proj_zesp;
+
+public class User {
+    private String email,password,first_name,last_name;
+
+    public User(String email, String password, String first_name, String last_name) {
+        this.email = email;
+        this.password = password;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+}
